ab2aed820f7862b18234f5877135a038bcf44a91
Kallol Khatua <kallolkhatua2005@gmail.com>
2025-11-09 01:30:04 +0530

    testing 2nd update automate deploy

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -10,16 +10,16 @@ public class TestController {
 
     @GetMapping("/first")
     public String first() {
-        return "first route update - 1";
+        return "first route update - 2";
     }
 
     @GetMapping("/second")
     public String second() {
-        return "second route update - 1";
+        return "second route update - 2";
     }
 
     @GetMapping("/third")
     public String third() {
-        return "third route update - 1";
+        return "third route update - 2";
     }
 }
